cfdbfa5da6adb402f7ff39eb490c46910d43cc1f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-18 15:09:28 +0200

    Create base.cs

---
+++ main/Models/work/base.cs
@@ -0,0 +1,10 @@
+﻿namespace main.Models.work {
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    public class @base {
+        [BsonId][BsonRepresentation(BsonType.ObjectId)] public string? _id { get; set; }
+        [BsonElement("title")] public string? title { get; set; }
+        [BsonElement("genre")] public string? genre { get; set; }
+        [BsonRepresentation(BsonType.DateTime)][BsonElement("date")] public DateTime? date { get; set; }
+    }
+}

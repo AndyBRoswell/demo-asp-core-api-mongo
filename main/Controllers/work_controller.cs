db7426cd249553596004a118c3431feb3adddd33
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-10-18 16:46:04 +0200

    /

---
+++ main/Controllers/work_controller.cs
@@ -0,0 +1,34 @@
+﻿using main.Services;
+using main.Models.work;
+using Microsoft.AspNetCore.Mvc;
+
+namespace main.Controllers {
+    [ApiController]
+    [Route("work")]
+    public class work_controller : Controller {
+        private readonly work_service _work_service;
+        public work_controller(work_service work_service) => _work_service = work_service;
+        [HttpGet] public async Task<List<work>> get() => await _work_service.get_async();
+        [HttpGet("{id:length(24)}")] public async Task<ActionResult<work>> get(string id) {
+            var work = await _work_service.get_async(id);
+            if (work == null) return NotFound();
+            return work;
+        }
+        [HttpPost] public async Task<ActionResult<work>> create(work work) {
+            await _work_service.create_async(work);
+            return CreatedAtAction(nameof(get), new { id = work._id }, work);
+        }
+        [HttpPut("{id:length(24)}")] public async Task<IActionResult> update(string id, work work_in) {
+            var work = await _work_service.get_async(id);
+            if (work == null) return NotFound();
+            await _work_service.update_async(id, work_in);
+            return NoContent();
+        }
+        [HttpDelete("{id:length(24)}")] public async Task<IActionResult> delete(string id) {
+            var work = await _work_service.get_async(id);
+            if (work == null) return NotFound();
+            await _work_service.delete_async(work._id);
+            return NoContent();
+        }
+    }
+}
--- main/Program.cs
+++ main/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<main.Models.work.database_settings>(builder.Configuration.GetSection("multimedia_database"));
-
+builder.Services.AddSingleton<main.Services.work_service>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
